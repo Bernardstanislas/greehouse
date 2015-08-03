de06b2477720cfa2785736e8c0911e739a0203f9
Stanislas Bernard <bernardstanislas@gmail.com>
2015-08-04 01:49:44 +0200

    Fix radio power

--- schemas/node/node.ino
+++ schemas/node/node.ino
@@ -1,217 +1,218 @@
-    // Include libraries
-    #include <DHT.h> // DHT sensor library
-    #include <LowPower.h> // low power library
-    #include <RFM69.h> // RFM69 radio library
-    #include <SPI.h> // SPI library
-    #include <avr/sleep.h> // sleep library
-    #include <stdlib.h> // library for maths
+// Include libraries
+#include <DHT.h> // DHT sensor library
+#include <LowPower.h> // low power library
+#include <RFM69.h> // RFM69 radio library
+#include <SPI.h> // SPI library
+#include <avr/sleep.h> // sleep library
+#include <stdlib.h> // library for maths
 
-    // Soil moisture sensor define
-    #define MOIST_PIN_1 6 // soil probe pin 1
-    #define MOIST_PIN_2 7 // soil probe pin 2
-    #define MOIST_PIN_READ A0 // analog read pin
-    #define MOIST_CYCLES 3 // how many times to read the moisture level. default is 3 times
+// Soil moisture sensor define
+#define MOIST_PIN_1 6 // soil probe pin 1
+#define MOIST_PIN_2 7 // soil probe pin 2
+#define MOIST_PIN_READ A0 // analog read pin
+#define MOIST_CYCLES 3 // how many times to read the moisture level. default is 3 times
 
-    // DHT Humidity + Temperature sensor define
-    #define DHT_PIN 5 // Data pin (D5) for DHT
-    #define DHT_PWR 4 // turn DHT on and off via transistor
-    #define DHT_TYPE DHT11 // sensor model DHT11
+// DHT Humidity + Temperature sensor define
+#define DHT_PIN 5 // Data pin (D5) for DHT
+#define DHT_PWR 4 // turn DHT on and off via transistor
+#define DHT_TYPE DHT11 // sensor model DHT11
 
-    DHT dht(DHT_PIN, DHT_TYPE); // define DHT11
+DHT dht(DHT_PIN, DHT_TYPE); // define DHT11
 
-    // Battery meter
-    #define VOLTAGE_PIN_READ A7 // analog voltage read pin for batery meter
-    #define VOLTAGE_PIN_READ_ENABLE 3 // current sink pin. ( enable voltage divider )
-    #define VOLTAGE_REF 3.3 // reference voltage on system. use to calculate voltage from ADC
-    #define VOLTAGE_DIVIDER 3.02 // if you have a voltage divider to read voltages, enter the multiplier here.
+// Battery meter
+#define VOLTAGE_PIN_READ A7 // analog voltage read pin for batery meter
+#define VOLTAGE_PIN_READ_ENABLE 3 // current sink pin. ( enable voltage divider )
+#define VOLTAGE_REF 3.3 // reference voltage on system. use to calculate voltage from ADC
+#define VOLTAGE_DIVIDER 3.02 // if you have a voltage divider to read voltages, enter the multiplier here.
 
-    // RADIO SETTINGS
-    // You will need to initialize the radio by telling it what ID it has and what network it's on
-    // The Node_ID takes values from 1-127, 0 is reserved for sending broadcast messages (send to all nodes)
-    // The Network ID takes values from 0-255
-    #define NODE_ID        20  // The ID of this node. Has to be unique. 1 is reserved for the gateway!
-    #define NETWORK_ID    10  //the network ID we are on
-    #define GATEWAY_ID     1  //the gateway Moteino ID (default is 1)
-    #define FREQUENCY     RF69_433MHZ
-    #define SECRET "sixteencharacter"
+// RADIO SETTINGS
+// You will need to initialize the radio by telling it what ID it has and what network it's on
+// The Node_ID takes values from 1-127, 0 is reserved for sending broadcast messages (send to all nodes)
+// The Network ID takes values from 0-255
+#define NODE_ID        20  // The ID of this node. Has to be unique. 1 is reserved for the gateway!
+#define NETWORK_ID    10  //the network ID we are on
+#define GATEWAY_ID     1  //the gateway Moteino ID (default is 1)
+#define FREQUENCY     RF69_433MHZ
+#define SECRET "sixteencharacter"
 
-    // Power Management Sleep cycles
-    #define SLEEP_CYCLE_DEFAULT 1 // Sleep cycle 450*8 seconds = 1 hour. DEFAULT 450
+// Power Management Sleep cycles
+#define SLEEP_CYCLE_DEFAULT 1 // Sleep cycle 450*8 seconds = 1 hour. DEFAULT 450
 
-    RFM69 radio; // Define radio
+RFM69 radio; // Define radio
 
-    void setup() {
-        // // Debug only
-        // Serial.begin(9600);
+void setup() {
+    // // Debug only
+    // Serial.begin(9600);
 
-        // Battery Meter setup
-        pinMode(VOLTAGE_PIN_READ, INPUT);
-        pinMode(VOLTAGE_PIN_READ_ENABLE, OUTPUT);
-        digitalWrite(VOLTAGE_PIN_READ_ENABLE, LOW);
+    // Battery Meter setup
+    pinMode(VOLTAGE_PIN_READ, INPUT);
+    pinMode(VOLTAGE_PIN_READ_ENABLE, OUTPUT);
+    digitalWrite(VOLTAGE_PIN_READ_ENABLE, LOW);
 
-        // Moisture sensor pin setup
-        pinMode(MOIST_PIN_1, OUTPUT);
-        pinMode(MOIST_PIN_2, OUTPUT);
-        pinMode(MOIST_PIN_READ, INPUT);
-        digitalWrite(MOIST_PIN_1, LOW);
+    // Moisture sensor pin setup
+    pinMode(MOIST_PIN_1, OUTPUT);
+    pinMode(MOIST_PIN_2, OUTPUT);
+    pinMode(MOIST_PIN_READ, INPUT);
+    digitalWrite(MOIST_PIN_1, LOW);
+    digitalWrite(MOIST_PIN_2, LOW);
+
+
+    // Humidity sensor setup
+    pinMode(DHT_PWR, OUTPUT);
+    digitalWrite(DHT_PWR, LOW);
+    dht.begin();
+
+    // Radio setup
+    radio.initialize(FREQUENCY,NODE_ID,NETWORK_ID);
+    radio.setHighPower();
+    radio.setPowerLevel(31);
+    radio.encrypt(SECRET);
+    radio.promiscuous(false);
+    radio.sleep();
+}
+
+void loop() {
+    int voltage;
+    readBatteryVoltage(voltage);
+
+    int moisture;
+    readMoisture(moisture);
+
+    int temperature;
+    int humidity;
+    readDht(temperature, humidity);
+
+    transmitMeasures(moisture, temperature, humidity, voltage);
+    //sleepOneHourMinusOneSecond();
+
+    // printDHT(temperature, humidity);
+    // printBatteryVoltage(voltage);
+    // printMoisture(moisture);
+    sleepEightSeconds();
+}
+
+// Battery voltage reading function
+// Reads the voltage three times, keeps the last reading
+void readBatteryVoltage(int& voltage) {
+    int voltageADC;
+    // Battery level check
+    digitalWrite(VOLTAGE_PIN_READ_ENABLE, HIGH); // turn on the battery meter
+    delay(20);
+    for ( int i = 0 ; i < 3 ; i++ ) {
+        delay(10); // delay, wait for circuit to stabalize
+        voltageADC = analogRead(VOLTAGE_PIN_READ); // read the voltage 3 times. keep last reading
+    }
+    digitalWrite(VOLTAGE_PIN_READ_ENABLE, LOW); // turn off the battery meter
+    voltage = voltageADC  * VOLTAGE_REF / 1023 * VOLTAGE_DIVIDER * 1000; // calculate the voltage
+}
+
+
+// Moisture sensor reading function
+// function reads 3 times and averages the data
+void readMoisture(int& moisture) {
+    // Soil Moisture sensor reading
+    int moistAvgValue = 0; // reset the moisture level before reading
+    for ( int moistReadCount = 0; moistReadCount < MOIST_CYCLES; moistReadCount++ ) {
+        int moistReadDelay = 88; // delay to reduce capacitive effects
+        // polarity 1 read
+        digitalWrite(MOIST_PIN_1, HIGH);
         digitalWrite(MOIST_PIN_2, LOW);
-
-
-        // Humidity sensor setup
-        pinMode(DHT_PWR, OUTPUT);
-        digitalWrite(DHT_PWR, LOW);
-        dht.begin();
-
-        // Radio setup
-        radio.initialize(FREQUENCY,NODE_ID,NETWORK_ID);
-        radio.setHighPower();
-        radio.encrypt(SECRET);
-        radio.promiscuous(false);
-        radio.sleep();
+        delay (moistReadDelay);
+        int moistVal1 = analogRead(MOIST_PIN_READ);
+        digitalWrite(MOIST_PIN_1, LOW);
+        delay (2);
+        // polarity 2 read
+        digitalWrite(MOIST_PIN_1, LOW);
+        digitalWrite(MOIST_PIN_2, HIGH);
+        delay (moistReadDelay);
+        int moistVal2 = analogRead(MOIST_PIN_READ);
+        //Make sure all the pins are off to save power
+        digitalWrite(MOIST_PIN_2, LOW);
+        digitalWrite(MOIST_PIN_1, LOW);
+        moistVal1 = 1023 - moistVal1; // invert the reading
+        moistAvgValue += (moistVal1 + moistVal2) / 2; // average readings. report the levels
     }
+    moisture = moistAvgValue / MOIST_CYCLES; // average the results
+}
 
-    void loop() {
-        int voltage;
-        readBatteryVoltage(voltage);
-
-        int moisture;
-        readMoisture(moisture);
-
-        int temperature;
-        int humidity;
-        readDht(temperature, humidity);
-
-        transmitMeasures(moisture, temperature, humidity, voltage);
-        //sleepOneHourMinusOneSecond();
-
-        // printDHT(temperature, humidity);
-        // printBatteryVoltage(voltage);
-        // printMoisture(moisture);
-        sleepEightSeconds();
+// DHT sensor reading function
+// Reads the humidity and temperature
+void readDht(int& temperature, int& humidity) {
+    // Humidity + Temperature sensor reading
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    digitalWrite(DHT_PWR, HIGH); // turn on sensor
+    delay (10000); // wait for sensor to stabilize
+    temperature = dht.readTemperature(); // read temperature as celsius
+    humidity = dht.readHumidity(); // read humidity
+    // check if returns are valid, if they are NaN (not a number) then something went wrong!
+    if (isnan(temperature) || isnan(humidity)) {
+        temperature = 0;
+        humidity = 0;
     }
+    delay (18);
+    digitalWrite(DHT_PWR, LOW); // turn off sensor
+}
 
-    // Battery voltage reading function
-    // Reads the voltage three times, keeps the last reading
-    void readBatteryVoltage(int& voltage) {
-        int voltageADC;
-        // Battery level check
-        digitalWrite(VOLTAGE_PIN_READ_ENABLE, HIGH); // turn on the battery meter
-        delay(20);
-        for ( int i = 0 ; i < 3 ; i++ ) {
-            delay(10); // delay, wait for circuit to stabalize
-            voltageADC = analogRead(VOLTAGE_PIN_READ); // read the voltage 3 times. keep last reading
-        }
-        digitalWrite(VOLTAGE_PIN_READ_ENABLE, LOW); // turn off the battery meter
-        voltage = voltageADC  * VOLTAGE_REF / 1023 * VOLTAGE_DIVIDER * 1000; // calculate the voltage
+// Transmit the measures by radio
+void transmitMeasures(int moisture, int temperature, int humidity, int voltage) {
+    // PREPARE READINGS FOR TRANSMISSION
+    String senseData = String(NODE_ID);
+    senseData += ":";
+    senseData += String(moisture);
+    senseData += ":";
+    senseData += String(temperature);
+    senseData += ":";
+    senseData += String(humidity);
+    senseData += ":";
+    senseData += String(voltage);
+    byte sendSize = senseData.length();
+    sendSize = sendSize + 1;
+    char sendBuf[sendSize];
+    senseData.toCharArray(sendBuf, sendSize); // convert string to char array for transmission
+    //Transmit the data
+    radio.send(GATEWAY_ID, sendBuf, strlen(sendBuf), false); // send the data
+    radio.sleep(); // sleep the radio to save power
+}
+
+// Sleep for one hour
+void sleepOneHourMinusOneSecond() {
+    // POWER MANAGEMENT DEEP SLEEP
+    // after everything is done, go into deep sleep to save power
+    int sleepCycle = 449; // Sleep cycle reset
+    for ( int sleepCycleCounter = 0; sleepCycleCounter < sleepCycle; sleepCycleCounter++ ) {
+        LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); //sleep duration is 8 seconds multiply by the sleep cycle variable.
     }
-
-
-    // Moisture sensor reading function
-    // function reads 3 times and averages the data
-    void readMoisture(int& moisture) {
-        // Soil Moisture sensor reading
-        int moistAvgValue = 0; // reset the moisture level before reading
-        for ( int moistReadCount = 0; moistReadCount < MOIST_CYCLES; moistReadCount++ ) {
-            int moistReadDelay = 88; // delay to reduce capacitive effects
-            // polarity 1 read
-            digitalWrite(MOIST_PIN_1, HIGH);
-            digitalWrite(MOIST_PIN_2, LOW);
-            delay (moistReadDelay);
-            int moistVal1 = analogRead(MOIST_PIN_READ);
-            digitalWrite(MOIST_PIN_1, LOW);
-            delay (2);
-            // polarity 2 read
-            digitalWrite(MOIST_PIN_1, LOW);
-            digitalWrite(MOIST_PIN_2, HIGH);
-            delay (moistReadDelay);
-            int moistVal2 = analogRead(MOIST_PIN_READ);
-            //Make sure all the pins are off to save power
-            digitalWrite(MOIST_PIN_2, LOW);
-            digitalWrite(MOIST_PIN_1, LOW);
-            moistVal1 = 1023 - moistVal1; // invert the reading
-            moistAvgValue += (moistVal1 + moistVal2) / 2; // average readings. report the levels
-        }
-        moisture = moistAvgValue / MOIST_CYCLES; // average the results
+    for ( int sleepCycleCounter = 0; sleepCycleCounter < 7; sleepCycleCounter++ ) {
+        LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF); //sleep duration is 8 seconds multiply by the sleep cycle variable.
     }
+}
 
-    // DHT sensor reading function
-    // Reads the humidity and temperature
-    void readDht(int& temperature, int& humidity) {
-        // Humidity + Temperature sensor reading
-        // Reading temperature or humidity takes about 250 milliseconds!
-        // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-        digitalWrite(DHT_PWR, HIGH); // turn on sensor
-        delay (10000); // wait for sensor to stabilize
-        temperature = dht.readTemperature(); // read temperature as celsius
-        humidity = dht.readHumidity(); // read humidity
-        // check if returns are valid, if they are NaN (not a number) then something went wrong!
-        if (isnan(temperature) || isnan(humidity)) {
-            temperature = 0;
-            humidity = 0;
-        }
-        delay (18);
-        digitalWrite(DHT_PWR, LOW); // turn off sensor
-    }
+// Sleep for eight seconds
+void sleepEightSeconds() {
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+}
 
-    // Transmit the measures by radio
-    void transmitMeasures(int moisture, int temperature, int humidity, int voltage) {
-        // PREPARE READINGS FOR TRANSMISSION
-        String senseData = String(NODE_ID);
-        senseData += ":";
-        senseData += String(moisture);
-        senseData += ":";
-        senseData += String(temperature);
-        senseData += ":";
-        senseData += String(humidity);
-        senseData += ":";
-        senseData += String(voltage);
-        byte sendSize = senseData.length();
-        sendSize = sendSize + 1;
-        char sendBuf[sendSize];
-        senseData.toCharArray(sendBuf, sendSize); // convert string to char array for transmission
-        //Transmit the data
-        radio.send(GATEWAY_ID, sendBuf, strlen(sendBuf), false); // send the data
-        radio.sleep(); // sleep the radio to save power
-    }
+/**
+* Print battery voltage.
+* @param  {float&} voltage       battery voltage
+*/
+void printBatteryVoltage(int& voltage) {
+    Serial.print("Battery voltage : ");
+    Serial.println(voltage);
+}
 
-    // Sleep for one hour
-    void sleepOneHourMinusOneSecond() {
-        // POWER MANAGEMENT DEEP SLEEP
-        // after everything is done, go into deep sleep to save power
-        int sleepCycle = 449; // Sleep cycle reset
-        for ( int sleepCycleCounter = 0; sleepCycleCounter < sleepCycle; sleepCycleCounter++ ) {
-            LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); //sleep duration is 8 seconds multiply by the sleep cycle variable.
-        }
-        for ( int sleepCycleCounter = 0; sleepCycleCounter < 7; sleepCycleCounter++ ) {
-            LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF); //sleep duration is 8 seconds multiply by the sleep cycle variable.
-        }
-    }
+// Print DHT measures
+void printDHT(int& temperature, int& humidity) {
+    Serial.print("Temperature : ");
+    Serial.println(temperature);
+    Serial.print("Humidity : ");
+    Serial.println(humidity);
+}
 
-    // Sleep for eight seconds
-    void sleepEightSeconds() {
-        LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-    }
-
-    /**
-     * Print battery voltage.
-     * @param  {float&} voltage       battery voltage
-     */
-    void printBatteryVoltage(int& voltage) {
-        Serial.print("Battery voltage : ");
-        Serial.println(voltage);
-    }
-
-    // Print DHT measures
-    void printDHT(int& temperature, int& humidity) {
-        Serial.print("Temperature : ");
-        Serial.println(temperature);
-        Serial.print("Humidity : ");
-        Serial.println(humidity);
-    }
-
-    // Print moisture measure
-    void printMoisture(int& moisture) {
-        Serial.print("Soil moisture : ");
-        Serial.println(moisture);
-    }
+// Print moisture measure
+void printMoisture(int& moisture) {
+    Serial.print("Soil moisture : ");
+    Serial.println(moisture);
+}
 
